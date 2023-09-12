c7ce35d4a394d47257726b4c20ad4978eb04c503
swetha <swethamuppirishetty1998@gmail.com>
2023-09-12 22:47:31 +0530

    Core Java

---
+++ CoreJava/AssignOperator.java
@@ -0,0 +1,18 @@
+// Assignment operator
+public class AssignOperator {
+    public static void main(String[] args) {
+
+        int a=4;
+        // a++; ++a
+        int result=a++;
+        System.out.println(result);
+        System.out.println(a);
+
+
+        // add,sub,mul,div and mod
+        // int a=10;
+        // int b=5;
+        // System.out.println(a%b);
+        // System.out.println(a/b);
+    }
+}
---
+++ CoreJava/ExampVariable.java
@@ -0,0 +1,22 @@
+public class ExampVariable {
+    public static void main(String[] args) {
+        // explicit conversion (or) casting
+    //    byte a=127;
+    //    int b=12;
+    //    a=(byte)b;
+    //    System.out.println(a);
+
+    //    implicit conversion
+    //    byte a=127;
+    //    int b=30;
+    //    b=a;
+    //    System.out.println(a);
+
+    // type promotion
+    byte a=10;
+    byte b=30;
+    int result = a*b;
+    System.out.println(result);
+
+    }
+}
